c49f5a03fdd598f1b6d6cef6c7a7e0fa9b8426a8
TipsukonCheer <tipsukon.cheer@mail.kmutt.ac.th>
2020-07-04 17:30:09 +0700

    [UPDATE] add routing path

--- client/index.js
+++ client/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client' //เป็นตัวstore dataที่ดึงมาจากgraphal server
 import {ApolloProvider} from 'react-apollo' //เป็นตัวเชื่อมreactกับapollo ก็คือมันจะเป็นตัวที่ไปดึงข้อมูลจาก ApolloClient
 import SongLists from './components/SongLists'
+import {Router,Route,hashHistory,IndexRoute} from 'react-router'
+import App from './components/App'
+import AddSong from './components/AddSong'
 
 //ในนี้เราไม่ต้องconfigอะไรเพิ่มเพราะappของเรามันชี้ไปที่serverของgraphqlอยู่แล้ว(ในserver.js)
 const client = new ApolloClient({})
@@ -10,9 +13,12 @@ const client = new ApolloClient({})
 const Root = () => {
   return (
   <ApolloProvider client={client}>
-    <div>
-      <SongLists/>
-    </div>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongLists}/>
+          <Route path="AddSong" component={AddSong}/>
+        </Route>
+      </Router>
   </ApolloProvider>
   );
 };
