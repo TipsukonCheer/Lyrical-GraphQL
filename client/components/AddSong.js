5a57e71c891e8370d136740c97dca95873d09cc1
TipsukonCheer <tipsukon.cheer@mail.kmutt.ac.th>
2020-07-04 19:28:42 +0700

    [UPDATE] add form

--- client/components/AddSong.js
+++ client/components/AddSong.js
@@ -3,18 +3,37 @@ import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
 
 class AddSong extends Component {    
+    constructor(props){
+        super(props);
+        this.state = {
+            title : ''
+        }
+    }
+
     render() {
-        console.log("props"+this.props)
+        console.log("props"+this.props.data)
+        console.log("title"+this.state.title)
         return (
             <div>
-                tttttt
-            </div>
+                <h1>Add new song</h1>
+                <form onSubmit={()=>console.log("submit")}>
+                    title
+                    <input
+                        onChange={(event)=>this.setState({title:event.target.value})}
+                        value={this.state.title}
+                    />
+                </form>
+             </div>
         )
     }
 }
 
 const query = gql`
-    
+{
+    songs{
+        title
+    }
+}
 `
 
 export default graphql(query)(AddSong);
